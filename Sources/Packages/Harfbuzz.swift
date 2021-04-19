24f8bc3031cd4d3b6c8664476dc507e378df3872
kojirou <kojirouhtc@gmail.com>
2021-04-19 20:11:52 +0800

    use cmake for harfbuzz

--- Sources/Packages/Harfbuzz.swift
+++ Sources/Packages/Harfbuzz.swift
@@ -1,22 +1,38 @@
 import BuildSystem
 
 public struct Harfbuzz: Package {
-  public init() {}
-  public func build(with env: BuildEnvironment) throws {
-    try env.changingDirectory("build", block: { _ in
-      try env.meson(
-        "--default-library=\(env.libraryType.mesonFlag)",
-        env.libraryType == .statik ? "-Db_lundef=false" : nil,
-        mesonFeatureFlag(false, "cairo"),
-        mesonFeatureFlag(true, "coretext"),
-        mesonFeatureFlag(true, "freetype"),
-        mesonFeatureFlag(false, "glib"),
-        mesonFeatureFlag(false, "gobject"),
-        mesonFeatureFlag(false, "graphite"),
-        mesonFeatureFlag(true, "icu"),
-        mesonFeatureFlag(false, "introspection")
-      )
 
+  public init() {}
+
+  public func build(with env: BuildEnvironment) throws {
+    try env.changingDirectory(env.randomFilename, block: { _ in
+//      try env.meson(
+//        "--default-library=\(env.libraryType.mesonFlag)",
+//        env.libraryType == .statik ? "-Db_lundef=false" : nil,
+//        mesonFeatureFlag(false, "cairo"),
+//        mesonFeatureFlag(true, "coretext"),
+//        mesonFeatureFlag(true, "freetype"),
+//        mesonFeatureFlag(false, "glib"),
+//        mesonFeatureFlag(false, "gobject"),
+//        mesonFeatureFlag(false, "graphite"),
+//        mesonFeatureFlag(true, "icu"),
+//        mesonFeatureFlag(false, "introspection"),
+//        ".."
+//      )
+
+      try env.cmake(
+        toolType: .ninja,
+        "..",
+        cmakeOnFlag(env.libraryType.buildShared, "BUILD_SHARED_LIBS"),
+        cmakeOnFlag(true, "HB_HAVE_CORETEXT"),
+        cmakeOnFlag(true, "HB_HAVE_FREETYPE"),
+        cmakeOnFlag(false, "HB_HAVE_GLIB"),
+        cmakeOnFlag(false, "HB_HAVE_GOBJECT"),
+        cmakeOnFlag(false, "HB_HAVE_GRAPHITE2"),
+        cmakeOnFlag(true, "HB_HAVE_ICU"),
+        cmakeOnFlag(false, "HB_HAVE_INTROSPECTION"),
+        nil
+        )
       try env.launch("ninja")
       try env.launch("ninja", "install")
     })
