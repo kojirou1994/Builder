8a0b008023ed35389a393d8aa78fbb9abc2af31d
kojirou <kojirouhtc@gmail.com>
2021-04-27 00:00:41 +0800

    add File

---
+++ Sources/Packages/File.swift
@@ -0,0 +1,41 @@
+import BuildSystem
+
+public struct File: Package {
+
+  public init() {}
+
+  public var defaultVersion: PackageVersion {
+    "5.40"
+  }
+
+  public func recipe(for order: PackageOrder) throws -> PackageRecipe {
+    let source: PackageSource
+    switch order.version {
+    case .head:
+      throw PackageRecipeError.unsupportedVersion
+    case .stable(let version):
+      source = .tarball(url: "https://astron.com/pub/file/file-\(version.toString(includeZeroPatch: false)).tar.gz")
+    }
+
+    return .init(
+      source: source,
+      dependencies: PackageDependencies(packages: [
+        .runTime(Zlib.self)
+      ])
+    )
+  }
+
+  public func build(with env: BuildEnvironment) throws {
+    try env.configure(
+      configureEnableFlag(false, CommonOptions.dependencyTracking),
+      env.libraryType.staticConfigureFlag,
+      env.libraryType.sharedConfigureFlag,
+      configureEnableFlag(true, "fsect-man5")
+    )
+
+    try env.make()
+
+    try env.make("install")
+  }
+
+}
