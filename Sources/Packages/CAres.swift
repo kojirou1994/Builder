8eab51072cbdb5bde90ba34216fa0529444185b7
kojirou <kojirouhtc@gmail.com>
2021-10-30 00:53:21 +0800

    update c-ares

--- Sources/Packages/CAres.swift
+++ Sources/Packages/CAres.swift
@@ -5,7 +5,7 @@ public struct CAres: Package {
   public init() {}
 
   public var defaultVersion: PackageVersion {
-    "1.17.1"
+    "1.18.1"
   }
 
   public func recipe(for order: PackageOrder) throws -> PackageRecipe {
