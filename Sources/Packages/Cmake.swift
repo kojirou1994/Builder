1fcaf01423d90a1b2f97bda96aff1970632968ba
kojirou <kojirouhtc@gmail.com>
2021-10-30 00:53:35 +0800

    update cmake

--- Sources/Packages/Cmake.swift
+++ Sources/Packages/Cmake.swift
@@ -5,7 +5,7 @@ public struct Cmake: Package {
   public init() {}
 
   public var defaultVersion: PackageVersion {
-    "3.21"
+    "3.21.4"
   }
 
   public func recipe(for order: PackageOrder) throws -> PackageRecipe {
