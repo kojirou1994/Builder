5a5b0990b28f752a309b2d4e6cb12109f93fed53
kojirou <kojirouhtc@gmail.com>
2021-10-30 01:21:14 +0800

    update libgit2

--- Sources/Packages/Libgit2.swift
+++ Sources/Packages/Libgit2.swift
@@ -5,7 +5,7 @@ public struct Libgit2: Package {
   public init() {}
 
   public var defaultVersion: PackageVersion {
-    "1.1.0"
+    "1.3"
   }
 
   public func recipe(for order: PackageOrder) throws -> PackageRecipe {
