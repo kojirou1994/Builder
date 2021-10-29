34f76cb7224917185d7c2f99bfd6c352074f30ab
kojirou <kojirouhtc@gmail.com>
2021-10-30 01:42:02 +0800

    update vpx

--- Sources/Packages/Vpx.swift
+++ Sources/Packages/Vpx.swift
@@ -5,7 +5,7 @@ public struct Vpx: Package {
   public init() {}
 
   public var defaultVersion: PackageVersion {
-    "1.10.0"
+    "1.11"
   }
 
   public func recipe(for order: PackageOrder) throws -> PackageRecipe {
