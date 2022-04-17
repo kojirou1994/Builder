0e417f688ae5b23271689207f02cd652862d400d
kojirou <kojirouhtc@gmail.com>
2022-04-17 19:01:38 +0800

    png: switch to cmake & ninja

--- Sources/Packages/Png.swift
+++ Sources/Packages/Png.swift
@@ -12,34 +12,41 @@ public struct Png: Package {
     let source: PackageSource
     switch order.version {
     case .head:
-      throw PackageRecipeError.unsupportedVersion
+      source = .repository(url: "https://github.com/glennrp/libpng.git")
     case .stable(let version):
       source = .tarball(url: "https://downloads.sourceforge.net/project/libpng/libpng16/\(version)/libpng-\(version.toString()).tar.xz")
     }
 
     return .init(
       source: source,
-      dependencies: [.runTime(Zlib.self)]
+      dependencies: [
+        .buildTool(Cmake.self),
+        .buildTool(Ninja.self),
+        .runTime(Zlib.self),
+      ]
     )
   }
 
   public func build(with context: BuildContext) throws {
+    try context.inRandomDirectory { _ in
+      try context.cmake(
+        toolType: .ninja,
+        "..",
+        cmakeDefineFlag(context.order.arch.isARM ? "on" : "off" , "PNG_ARM_NEON"),
+        cmakeOnFlag(context.libraryType.buildStatic, "PNG_STATIC"),
+        cmakeOnFlag(context.libraryType.buildShared, "PNG_SHARED"),
+        cmakeDefineFlag(context.prefix.lib.path, "CMAKE_INSTALL_NAME_DIR")
+      )
 
-    try context.fixAutotoolsForDarwin()
+      try context.make(toolType: .ninja)
 
-    try context.configure(
-      configureEnableFlag(false, CommonOptions.dependencyTracking),
-      context.libraryType.staticConfigureFlag,
-      context.libraryType.sharedConfigureFlag
-    )
+      if context.canRunTests {
+        try context.make(toolType: .ninja, "test")
+      }
 
-    try context.make()
+      try context.make(toolType: .ninja, "install")
 
-    if context.canRunTests {
-      try context.make("test")
     }
-
-    try context.make("install")
   }
 
 }
