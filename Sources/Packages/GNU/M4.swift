75e1818b166cec829d6b16d8b6ae5803c5cb193a
kojirou <kojirouhtc@gmail.com>
2023-04-16 03:18:42 +0800

    update m4

--- Sources/Packages/GNU/M4.swift
+++ Sources/Packages/GNU/M4.swift
@@ -5,7 +5,7 @@ public struct M4: Package {
   public init() {}
 
   public var defaultVersion: PackageVersion {
-    "1.4.18"
+    "1.4.19"
   }
 
   public func recipe(for order: PackageOrder) throws -> PackageRecipe {
@@ -20,17 +20,9 @@ public struct M4: Package {
     let source: PackageSource
     switch order.version {
     case .head:
-      throw PackageRecipeError.unsupportedVersion
+      source = .repository(url: "http://git.savannah.gnu.org/r/m4.git")
     case .stable(let version):
-      var patches = [PackagePatch]()
-      if order.system == .linuxGNU {
-        patches.append(
-          .remote(
-            url: "https://raw.githubusercontent.com/archlinux/svntogit-packages/19e203625ecdf223400d523f3f8344f6ce96e0c2/trunk/m4-1.4.18-glibc-change-work-around.patch",
-            sha256: "fc9b61654a3ba1a8d6cd78ce087e7c96366c290bc8d2c299f09828d793b853c8"))
-      }
-      source = .tarball(url: "https://ftp.gnu.org/gnu/m4/m4-\(version.toString()).tar.xz",
-                        patches: patches)
+      source = .tarball(url: "https://ftp.gnu.org/gnu/m4/m4-\(version.toString()).tar.xz")
     }
 
     return .init(
@@ -41,9 +33,7 @@ public struct M4: Package {
 
   public func build(with context: BuildContext) throws {
 
-    try context.configure(
-
-    )
+    try context.configure()
 
     try context.make()
     try context.make("install")
